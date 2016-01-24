955731d56d7fb4304a4303987f32abbd61b35928
Vitchyr Pong <vitchyr@gmail.com>
2016-01-24 00:22:13 -0500

    add own constants file

--- analyze_nn_sarsa.lua
+++ analyze_nn_sarsa.lua
@@ -5,7 +5,7 @@ require 'constants'
 local as = require 'analyze_sarsa'
 
 local fe = BlackJackOneHotSAFE()
-local explorer = rl.ConstExplorer(EPS)
-local factory = rl.NNSarsaFactory(nil, nil, explorer, fe, STEP_SIZE)
+local explorer = rl.ConstExplorer(DEFAULT_EXPLORE_EPS)
+local factory = rl.NNSarsaFactory(nil, nil, explorer, fe, DEFAULT_STEP_SIZE)
 factory:set_feature_extractor(fe)
 as.analyze_sarsa(factory, 'nn_sarsa')
